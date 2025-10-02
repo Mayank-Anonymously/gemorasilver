d7c59336397d1dde890b16edcf9ebae40b26989e
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-10-02 19:48:33 +0530

    added changes

--- component/menu/MobileMenu.js
+++ component/menu/MobileMenu.js
@@ -3,13 +3,76 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MobileMenu({ show, handleClose }) {
 	const categories = [
-		'Rings',
-		'Earrings',
-		'Pendants',
-		'Mangalsutra',
-		'Bracelets',
-		'Set (Pendants + Earrings)',
-		'Toe Rings',
+		{
+			name: 'Bracelets',
+			link: 'bracelet',
+			count: 28,
+			description: 'Explore our handcrafted bracelets made with love.',
+			images: ['/assets/product/bracelet.png', '/assets/product/bracelet.png'],
+		},
+
+		{
+			name: 'Rings',
+			link: 'rings',
+			count: 48,
+			description: 'Explore our beautiful rings collection.',
+			images: ['/assets/category/rings.png', '/assets/category/rings.png'],
+		},
+		{
+			name: 'Earrings',
+			link: 'earrings',
+			count: 18,
+			description: 'Find earrings to match every style.',
+			images: [
+				'/assets/category/earrings.png',
+				'/assets/category/earrings.png',
+			],
+		},
+		{
+			name: 'Pendants',
+			link: 'pendant',
+			count: 14,
+			description: 'Elegant pendants for every occasion.',
+			images: ['/assets/category/pendant.png', '/assets/category/pendant.png'],
+		},
+
+		{
+			name: 'Jewellery Set',
+			link: 'jewellery-set',
+			count: 13,
+			description: 'Beautiful matching sets of pendants and earrings.',
+			images: [
+				'/assets/category/jewellery-set.png',
+				'/assets/category/jewellery-set.png',
+			],
+		},
+		{
+			name: 'Necklace Set',
+			link: 'necklace-set',
+			count: 13,
+			description: 'Beautiful matching sets of pendants and earrings.',
+			images: [
+				'/assets/category/necklace-set.png',
+				'/assets/category/necklace-set.png',
+			],
+		},
+		{
+			name: 'Toe Rings',
+			link: 'toe-ring',
+			count: 13,
+			description: 'Stylish toe rings for every occasion.',
+			images: [
+				'/assets/category/toe-rings.png',
+				'/assets/category/toe-rings.png',
+			],
+		},
+		{
+			name: 'Anklets',
+			link: 'anklet',
+			count: 3,
+			description: 'Stylish toe rings for every occasion.',
+			images: ['/assets/category/anklet.png', '/assets/category/anklet.png'],
+		},
 	];
 
 	return (
@@ -29,9 +92,8 @@ function MobileMenu({ show, handleClose }) {
 					{categories.map((category, idx) => (
 						<li
 							key={idx}
-							className='mobile-menu-category-item'
-							onClick={() => console.log(`${category} clicked`)}>
-							{category}
+							className='mobile-menu-category-item'>
+							<a href={`/category/${category.link}`}>{category.name}</a>
 						</li>
 					))}
 				</ul>
